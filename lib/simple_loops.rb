e70d9cf9324755bc9279d726aadaca1e5f78a753
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-04-01 19:01:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,16 @@
-# Write your methods here
+
+#def method_name(parameter)
+#  count = 0
+#  while count < (something) or array.length do or limit or # etc
+#  puts message or puts array [count]
+#    count += 1
+#  end
+# end
+
+def loop_message_five_times
+  count = 0
+  while count < 5 do
+    puts array[count]
+    count += 1
+    end
+  end
