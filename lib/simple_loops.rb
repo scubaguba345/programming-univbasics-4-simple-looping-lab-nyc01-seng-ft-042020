eca1111a69d4b659115f9db6cbe9cb5f84800249
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-04-01 19:36:27 +0000

    Done.

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,43 @@
-# Write your methods here
+
+#def method_name(parameter)
+#  count = 0
+#  while count < (something) or array.length do or limit or # etc
+#  puts message or puts array [count]
+#    count += 1
+#  end
+# end
+
+def loop_message_five_times(message)
+  message == 'Hello World'
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+    end
+  end
+
+def loop_message_n_times(message, number)
+  count = 0
+  while count < number do
+    puts message
+    count += 1
+    end
+  end
+
+def output_array(array)
+  count = 0
+  while count < array.length do
+    puts array[count]
+    count += 1
+    end
+  end
+
+def return_string_array(array)
+  count = 0
+  new_array = []
+  while count < array.length do
+    new_array.push (array[count].to_s)
+    count += 1
+  end
+  new_array
+  end
