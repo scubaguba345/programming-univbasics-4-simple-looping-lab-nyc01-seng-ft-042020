067173d484289cca0d054231bae9f890330667df
brandon gubitosa <brandon.gubitosa@gmail.com>
2020-04-01 19:14:47 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -19,7 +19,6 @@ def loop_message_five_times(message)
 def loop_message_n_times(message, limit)
   message == 'Hello Moon'
   count = 0
-  limit = 5
   while count < limit do
     puts message
     count += 1
